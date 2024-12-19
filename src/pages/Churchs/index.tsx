d31a4a7c2f0d07b23099045b8beee556ed52e75d
giovannicurcuruto <ggcurcuruto@gmail.com>
2024-12-19 17:56:35 -0300

    igrejas respondendo da API

--- src/pages/Churchs/index.tsx
+++ src/pages/Churchs/index.tsx
@@ -1,9 +1,59 @@
+import { useEffect, useState } from "react";
 import { BannerEncontre } from "../../components/Banners";
 import FoundChurchs from "../../components/FoundChurchs";
+import { Locations } from "../../types/churchs/locations";
 import { AboutContent, GridSection } from "../Churchs/style";
-import { locations } from './location';
+import { AuthDataInfo } from "../../utils/auth";
+import axios from "axios";
+
+
+type ApiResponse = {
+    success: boolean;
+    data: {
+        Locations: Locations[];
+
+    };
+};
+
 
 export default function ChurchsPage() {
+    const [locale, setLocale] = useState<Locations[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get<ApiResponse>(
+                    AuthDataInfo.URL + "rest.php?class=SiteService&method=getLocalizacaoItens",
+                    {
+                        headers: {
+                            Authorization: AuthDataInfo.TOKEN,
+                        }
+                    }
+                );
+                //console.log(response.data.data)
+                if (response.data.success) {
+                    const localeData = response.data.data;
+                    setLocale(localeData.Locations);
+
+                } else {
+                    console.error("Erro ao carregar os dados da API");
+                }
+            } catch (error) {
+                if (axios.isAxiosError(error)) {
+                    console.error("Erro Axios:", error.response?.data || error.message);
+                } else {
+                    console.error("Erro inesperado:", error);
+                }
+            }
+        };
+
+        fetchData();
+    }, []);
+    console.log(locale.length);
+    
+
+
+
     return (
         <>
             <BannerEncontre />
@@ -11,13 +61,13 @@ export default function ChurchsPage() {
                 <h1>Encontre uma igreja mais proxima de você</h1>
                 <h4>Encontre a Mais de Cristo mais perto de você</h4>
                 <GridSection>
-                {locations.map((church, index) => (
-                        <FoundChurchs 
+                    {locale.map((church, index) => (
+                        <FoundChurchs
                             key={index} // Sempre use "key" única ao renderizar listas
-                            name={church.name} 
-                            address={church.address} 
-                            city={church.city} 
-                            cults={church.cults} 
+                            name={church.pastores}
+                            address={church.endereco}
+                            city={church.cidade}
+                            cults={church.cultos}
                         />
                     ))}
 
---
+++ src/types/churchs/locations.ts
@@ -0,0 +1,11 @@
+export type Locations = {
+    id: number,
+    pastores: string,
+    endereco: string,
+    cidade: string,
+    cultos: string,
+    latitude: string,
+    longitude: string,
+    imagem: string
+
+}
